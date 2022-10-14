957654bf4b07c809bac38062e64d0707c543573e
Hamza1610 <muhammadhamza163003@gmailcom>
2022-10-13 21:47:22 -0700

    puts file for making output of strings

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>  
+#include <string.h>    
+int main(){    
+	char name = "Programming is like building a multilingual puzzle";    
+	puts("%c",name);  //displays string    
+	return 0;    
+}  
