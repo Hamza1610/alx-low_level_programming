a1811201de6e16a8fe5be7bd5a2606bb9441dd3c
Hamza1610 <muhammadhamza163003@gmailcom>
2022-10-16 14:29:09 -0700

    6-size.c completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	float f;
 
 	/* statements **/
-	printf("Size of a char: %c byte(s)\n", sizeof(ch));
+	printf("Size of a char: %zu byte(s)\n", sizeof(ch));
 	printf("Size of a int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("Size of a float: %f byte(s)\n", sizeof(f));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
