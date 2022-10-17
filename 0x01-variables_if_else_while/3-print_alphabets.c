781167316704f41116f064d3824447426b80dccc
Hamza1610 <muhammadhamza163003@gmailcom>
2022-10-17 09:54:49 -0700

    created files for working

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+/**
+ * main - print alphabet in lowercase and uppercase
+ * Return: void
+*/
+int main(void)
+{
+	char low_alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	char up_alpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i, j;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(low_alpha[i]);
+	}
+	for (j = 0; j < 26; j++)
+	{
+		putchar(up_alpha[j]);
+	}
+	putchar('\n');
+	return (0);
+}
