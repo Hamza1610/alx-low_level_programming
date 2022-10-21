493876e2f794c41805168fd1b2bcdb86cdd8c5d0
Hamza1610 <muhammadhamza162003@gmail.com>
2022-10-20 21:26:42 -0700

    Files ceated for projects

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int _mul(int, int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
