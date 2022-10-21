f1955bfead2b327296fd025ae0427c3299986b61
Hamza1610 <muhammadhamza162003@gmail.com>
2022-10-20 19:09:13 -0700

    Configure to the betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * Create a function with name _isupper()
+ * _isupper -  checks if c is an upercase
  * Return 1 if "c" is uppercase
  * returns 0 otherwise
+ * c - is an int variable passed into the function _isupper()
 */
 int _isupper(int c)
 {
@@ -14,4 +15,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-}	
+}
