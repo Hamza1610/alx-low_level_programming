9f162ff06e6a29e3bfb3302786c2d98c1157c283
Hamza1610 <muhammadhamza162003@gmail.com>
2022-10-20 20:35:11 -0700

    sleepless nightof file creation and coding

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,13 @@
 /* Header declaraation goes here */
 int _putchar(char c);
 int _isupper(int c);
-
+int _isdigit(int c);
+int _mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 #endif
