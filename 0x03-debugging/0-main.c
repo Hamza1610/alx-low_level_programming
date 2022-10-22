c6b500c10256bb757d6c4117ccbd3b6fc8749554
Hamza1610 <muhammadhamza162003@gmail.com>
2022-10-21 21:03:25 -0700

    file contente chaned

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,8 @@
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
+* i : argument for function postive_or_negative
+* void is a non given
 */
 
 int main(void)
