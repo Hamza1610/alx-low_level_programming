0da7388f363412853bbb5fa2a05a2c79ea0c1b63
Hamza1610 <muhammadhamza162003@gmail.com>
2022-10-21 19:52:43 -0700

    inspected and edited for betty style feature

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - nmbr fct
- *
+ * Bwave ICT file
  * Return: no return
  */
 
@@ -15,11 +15,9 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
-			}
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
 			_putchar('\n');
-	}
+		}
 }
